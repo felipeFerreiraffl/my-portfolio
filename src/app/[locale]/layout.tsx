53e3cf8522d6b3543b0d105e084da4fc360f72e1
felipeFerreiraffl <felipado2x0@gmail.com>
2026-07-02 19:30:17 -0300

    feat: new animation on main button dots

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import { routing } from "@/libs/i18n/routing";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@teispace/next-themes";
 import { getTheme } from "@teispace/next-themes/server";
+import { domAnimation, LazyMotion } from "motion/react";
 import { Metadata } from "next";
 import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
@@ -48,25 +49,27 @@ export default async function RootLayout({ children, params }: LocaleRoutingProp
 
   return (
     <html lang={locale} className={`${spaceGrotesk.className}`} suppressHydrationWarning>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          initialTheme={initialTheme ?? "light"}
-          themes={availableThemes}
-          transition={{
-            type: "fade",
-            duration: 300,
-            origin: "center",
-          }}>
-          <NextIntlClientProvider
-            locale={locale}
-            messages={plainMessages}
-            now={new Date()}
-            timeZone="America/Sao_Paulo">
-            {children}
-          </NextIntlClientProvider>
-        </ThemeProvider>
-      </body>
+      <LazyMotion features={domAnimation}>
+        <body>
+          <ThemeProvider
+            attribute="class"
+            initialTheme={initialTheme ?? "light"}
+            themes={availableThemes}
+            transition={{
+              type: "fade",
+              duration: 300,
+              origin: "center",
+            }}>
+            <NextIntlClientProvider
+              locale={locale}
+              messages={plainMessages}
+              now={new Date()}
+              timeZone="America/Sao_Paulo">
+              {children}
+            </NextIntlClientProvider>
+          </ThemeProvider>
+        </body>
+      </LazyMotion>
     </html>
   );
 }
