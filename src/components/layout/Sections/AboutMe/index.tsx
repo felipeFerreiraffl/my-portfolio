2fce6a43da4be2e0ca5a42b36d154dd70c323de3
felipeFerreiraffl <felipado2x0@gmail.com>
2026-07-10 18:10:04 -0300

    chore(items): removed once prop on topic item animation

--- src/components/layout/Sections/AboutMe/index.tsx
+++ src/components/layout/Sections/AboutMe/index.tsx
@@ -25,7 +25,7 @@ export default function AboutMe({ ref }: SectionProps) {
   const itemVars: Variants = {
     hidden: {
       opacity: 0,
-      y: 8,
+      y: 16,
     },
     visible: {
       opacity: 1,
@@ -43,7 +43,7 @@ export default function AboutMe({ ref }: SectionProps) {
         variants={containerVars}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
+        viewport={{ amount: 0.5 }}
         className="grid lg:grid-cols-3 grid-cols-1 gap-5 md:px-12 px-7">
         <AboutTopic variants={itemVars} title={tSec("hobbies.title")}>
           <ul className="flex xl:flex-row flex-col gap-5">
