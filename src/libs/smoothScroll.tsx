7b62ff0b4187f908ef20746ecef9f66395ef08b5
felipeFerreiraffl <felipado2x0@gmail.com>
2026-07-10 18:12:22 -0300

    chore(scroll): set wheelMultiplier to 1

--- src/libs/smoothScroll.tsx
+++ src/libs/smoothScroll.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from "react";
 
 export default function SmoothScrollProvider({ children }: { children: ReactNode }) {
   return (
-    <ReactLenis root options={{ duration: 1.2, smoothWheel: true, wheelMultiplier: 1.2 }}>
+    <ReactLenis root options={{ duration: 1.2, smoothWheel: true, wheelMultiplier: 1 }}>
       {children}
     </ReactLenis>
   );
