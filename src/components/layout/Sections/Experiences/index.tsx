afce4b35f9da1cbdf407aa074a8338bda5765bbc
felipeFerreiraffl <felipado2x0@gmail.com>
2026-07-13 22:28:47 -0300

    feat: new first structure for experiences data and section

---
+++ src/components/layout/Sections/Experiences/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Section from "@/components/ui/Section";
+import { SectionProps } from "@/types/elements/elements.types";
+import { useTranslations } from "next-intl";
+
+export default function Experiences({ ref }: SectionProps) {
+  const tSec = useTranslations("Experiences");
+
+  return <Section ref={ref} title={tSec("title")}></Section>;
+}
--- src/types/elements/data.types.ts
+++ src/types/elements/data.types.ts
@@ -1,8 +1,28 @@
-interface HeroRingData {
+import { SVGIcon } from "./elements.types";
+
+export interface HeroRingData {
   id: string;
   sizeVar: string;
   borderWidth: number;
   borderStyle?: string;
   className: string;
   hasGradient?: boolean;
+}
+
+export interface ExperienceData {
+  id: `exp-${number}`;
+  title: string;
+  icon: SVGIcon;
+  place: string;
+  initialTime: {
+    month: string;
+    year: number;
+  };
+  endingTime?: {
+    month: string;
+    year: number;
+  };
+  type: "academic" | "work";
+  description: string;
+  skills: string[];
 }
