abaffb3e31476ea241f06c325d47558fa5afebe5
felipeFerreiraffl <felipado2x0@gmail.com>
2026-07-23 20:48:14 -0300

    style(skills): position adjustment of elements Replaced size-* with max-w-*, w-full and h-full to show all elements inside the circle. Also changed max-width of title line.

--- src/components/layout/Sections/Skills/SkillSet.tsx
+++ src/components/layout/Sections/Skills/SkillSet.tsx
@@ -12,7 +12,13 @@ type SkillSetProps = Omit<SkillsData, "id"> & {
   circleRef?: Ref<HTMLDivElement>;
 };
 
-export default function SkillSet({ title, skills, illustration, reversed, circleRef }: SkillSetProps) {
+export default function SkillSet({
+  title,
+  skills,
+  illustration,
+  reversed,
+  circleRef,
+}: SkillSetProps) {
   const tSec = useTranslations("Skills");
 
   return (
@@ -21,7 +27,7 @@ export default function SkillSet({ title, skills, illustration, reversed, circle
         "relative lg:max-w-[60dvw] w-full mx-auto flex items-center flex-col lg:gap-0 gap-2",
         reversed ? "lg:flex-row-reverse lg:translate-x-[22%]" : "lg:flex-row",
       )}>
-      <div className="flex flex-col w-full">
+      <div className="flex flex-col w-full max-w-75">
         <h3
           className={cn(
             "md:text-[2rem] text-xl font-bold leading-heading text-title text-center pl-0",
@@ -31,7 +37,10 @@ export default function SkillSet({ title, skills, illustration, reversed, circle
         </h3>
 
         <div
-          className={cn("pointer-events-none lg:flex hidden items-center -mt-0.5", reversed && "flex-row-reverse")}>
+          className={cn(
+            "pointer-events-none lg:flex hidden items-center -mt-0.5",
+            reversed && "flex-row-reverse",
+          )}>
           <div className="grid place-items-center size-2 border-[0.5px] border-main rounded-full">
             <div className="size-1 bg-main rounded-full" />
           </div>
@@ -42,7 +51,7 @@ export default function SkillSet({ title, skills, illustration, reversed, circle
 
       <div
         ref={circleRef}
-        className="lg:size-94 size-62 grid place-items-center lg:grid-cols-3 grid-cols-2 gap-y-5 gap-x-8 lg:py-25 py-9 px-15 border border-main rounded-full shrink-0">
+        className="max-w-95 h-full w-full aspect-square grid place-items-center lg:grid-cols-3 grid-cols-2 gap-y-5 gap-x-8 lg:py-25 py-9 px-15 border border-main rounded-full shrink-0">
         {skills.map((s) => (
           <div key={s.label} className="relative grid place-items-center md:size-16 size-12">
             <Icon icon={s.icon} className="md:size-12 size-8 text-title" ariaLabel={s.label} />
