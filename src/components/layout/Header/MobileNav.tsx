06e8174a8003303d1235dab81c06fb5efdbca2a1
felipeFerreiraffl <felipado2x0@gmail.com>
2026-07-28 21:16:36 -0300

    refactor(nav): change button styles with icon inside in MobileNav

--- src/components/layout/Header/MobileNav.tsx
+++ src/components/layout/Header/MobileNav.tsx
@@ -28,21 +28,19 @@ export default function MobileNav() {
   };
 
   return (
-    <nav className="md:hidden w-[90dvw] fixed bottom-10 left-1/2 -translate-x-1/2 flex items-center justify-between bg-main/20 border-[0.5px] border-main rounded-[40px] py-3 px-5 backdrop-blur-md">
+    <nav className="md:hidden max-w-[90dvw] w-full fixed bottom-10 left-1/2 -translate-x-1/2 flex items-center justify-between bg-main/20 border-[0.5px] border-main rounded-[40px] py-3 px-5 backdrop-blur-md">
       <ThemeChanger />
       <ul className="flex items-center gap-3">
         {NAV_LINKS.map((link) => (
           <li key={link.id}>
             <button
               onClick={() => handleScrollToSection(resolveIdToRef(link.id))}
-              aria-label={t(link.label)}>
-              <Icon
-                icon={link.icon}
-                className={cn(
-                  "p-2 rounded-full text-text size-8",
-                  activeSec === link.id ? "bg-main/60" : "bg-main/20",
-                )}
-              />
+              aria-label={t(link.label)}
+              className={cn(
+                "grid place-items-center size-9 rounded-full transition-colors duration-300 shrink-0",
+                activeSec === link.id ? "bg-main/60" : "bg-main/20",
+              )}>
+              <Icon icon={link.icon} className={cn("text-text size-5")} />
             </button>
           </li>
         ))}
