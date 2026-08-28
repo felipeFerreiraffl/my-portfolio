0e1bcaab468d522f15df36d17d6672c6d0695fdc
felipeFerreiraffl <felipado2x0@gmail.com>
2026-08-27 21:25:44 -0300

    refactor(projects): changed project 3 for different link, demo and informations

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,14 +1,16 @@
-import ProjFallbackLight from "@/assets/img/projects/preview-fallback-light.png";
-import ProjFallabackDark from "@/assets/img/projects/preview-fallback-dark.png";
-import ProjCalc1 from "@/assets/img/projects/calc/calc-1.png";
-import ProjCalc2 from "@/assets/img/projects/calc/calc-2.png";
-import ProjCalc3 from "@/assets/img/projects/calc/calc-3.png";
-import ProjCalc4 from "@/assets/img/projects/calc/calc-4.png";
 import ProjAniMan1 from "@/assets/img/projects/animan/animan-1.png";
 import ProjAniMan2 from "@/assets/img/projects/animan/animan-2.png";
 import ProjAniMan3 from "@/assets/img/projects/animan/animan-3.png";
 import ProjAniMan4 from "@/assets/img/projects/animan/animan-4.png";
 import ProjAniMan5 from "@/assets/img/projects/animan/animan-5.png";
+import ProjCalc1 from "@/assets/img/projects/calc/calc-1.png";
+import ProjCalc2 from "@/assets/img/projects/calc/calc-2.png";
+import ProjCalc3 from "@/assets/img/projects/calc/calc-3.png";
+import ProjCalc4 from "@/assets/img/projects/calc/calc-4.png";
+import ProjFallabackDark from "@/assets/img/projects/preview-fallback-dark.png";
+import ProjFallbackLight from "@/assets/img/projects/preview-fallback-light.png";
+import ProjTSAuth1 from "@/assets/img/projects/tsauth/tsauth-1.png";
+import ProjTSAuth2 from "@/assets/img/projects/tsauth/tsauth-2.png";
 
 export const IMAGES = {
   projects: {
@@ -28,6 +30,10 @@ export const IMAGES = {
       img_3: ProjAniMan3,
       img_4: ProjAniMan4,
       img_5: ProjAniMan5,
+    },
+    proj_3: {
+      img_1: ProjTSAuth1,
+      img_2: ProjTSAuth2,
     },
   },
 } as const;
